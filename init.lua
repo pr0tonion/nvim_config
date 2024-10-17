f595f242cb5dd6fd71b04559ca3613e79a0c3a8e
pr0tonion <marcus.pedersen95@gmail.com>
2024-10-17 20:31:36 +0200

    bla

--- init.lua
+++ init.lua
@@ -2,7 +2,10 @@
 vim.g.mapleader = " "
 vim.o.clipboard = "unnamedplus"
 vim.wo.number = true
--- vim.wo.relativenumber = true
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.termguicolors = true
+vim.wo.relativenumber = true
 
 -- Tab settings
 vim.opt.tabstop = 4
