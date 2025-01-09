07ba9d56fd28ae8fd233455abd09a1861ce360b7
marcuspedersen <kHFdhLpsfH2fpjKPuVB9>
2025-01-09 09:47:40 +0100

    Added code completion and a few more keymappings

--- lua/config/lsp-config.lua
+++ lua/config/lsp-config.lua
@@ -3,17 +3,21 @@ local cmp_nvim_lsp = require('cmp_nvim_lsp')
 
 
 require("mason-lspconfig").setup {
-    ensure_installed = { "lua_ls", "pyright" },
+	ensure_installed = { "lua_ls", "pyright" },
 }
 
 require("mason-lspconfig").setup_handlers({
-    function(server_name)  -- Default handler (for all servers)
-        lspconfig[server_name].setup({
-            capabilities = cmp_nvim_lsp.default_capabilities(),  -- Enable completion capabilities
-        })
-    end,
+	function(server_name)                                       -- Default handler (for all servers)
+		lspconfig[server_name].setup({
+			capabilities = cmp_nvim_lsp.default_capabilities(), -- Enable completion capabilities
+		})
+	end,
 })
 
 lspconfig.pyright.setup({
-  capabilities = require('cmp_nvim_lsp').default_capabilities(),  -- Attach nvim-cmp capabilities to LSP
+	capabilities = require('cmp_nvim_lsp').default_capabilities(), -- Attach nvim-cmp capabilities to LSP
 })
+
+map("n", "<leader>cn", vim.diagnostic.goto_next, { desc = "Go to next error" })
+map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
+map("n", "<leader>cc", vim.lsp.buf.completion, { desc = "Code completion" })
