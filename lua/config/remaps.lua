ef06760874d4da910cb9d70ea64d6140f8ebea1d
marcuspedersen <kHFdhLpsfH2fpjKPuVB9>
2025-01-30 20:08:46 +0100

    Checker update, remaps, and tmux navigator

--- lua/config/remaps.lua
+++ lua/config/remaps.lua
@@ -1,17 +1,17 @@
 local function map(mode, lhs, rhs, opts)
-  local options = { noremap=true, silent=true }
-  if opts then
-    options = vim.tbl_extend('force', options, opts)
-  end
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+	local options = { noremap = true, silent = true }
+	if opts then
+		options = vim.tbl_extend('force', options, opts)
+	end
+	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 -- Explorer
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Change split orientation
-map('n', '<leader>tk', '<C-w>t<C-w>K', {desc = "Tab vertical to horizontal"}) -- change vertical to horizontal
-map('n', '<leader>th', '<C-w>t<C-w>H', {desc = "Tab Horizontal to vertical"}) -- change horizontal to verticai 
+map('n', '<leader>tk', '<C-w>t<C-w>K', { desc = "Tab vertical to horizontal" }) -- change vertical to horizontal
+map('n', '<leader>th', '<C-w>t<C-w>H', { desc = "Tab Horizontal to vertical" }) -- change horizontal to verticai
 
 -- Move around splits using Ctrl + {h,j,k,l}
 map('n', '<C-h>', '<C-w>h')
@@ -19,6 +19,11 @@ map('n', '<C-j>', '<C-w>j')
 map('n', '<C-k>', '<C-w>k')
 map('n', '<C-l>', '<C-w>l')
 
+--Tmux
+map('n', '<C-h>', '<cmd> TmuxNavigateLeft<CR>')
+map('n', '<C-j>', '<cmd> TmuxNavigateDown<CR>')
+map('n', '<C-k>', '<cmd> TmuxNavigateUp<CR>')
+map('n', '<C-l>', '<cmd> TmuxNavigateRight<CR>')
 -- Disable arrow keys
 map('n', '<Up>', '<Nop>')
 map('n', '<Down>', '<Nop>')
@@ -33,7 +38,7 @@ map('n', 'J', '10j') -- Shift + j to move down 10 lines
 map('n', 'K', '10k') -- Shift + k to move up 10 lines
 
 -- NvimTree
-map('n', '<leader><tab>', ':NvimTreeToggle<CR>')            -- open/close
+map('n', '<leader><tab>', ':NvimTreeToggle<CR>') -- open/close
 --map('n', '<leader>f', ':NvimTreeRefresh<CR>')       -- refresh
 --map('n', '<leader>n', ':NvimTreeFindFile<CR>')      -- search file
 
